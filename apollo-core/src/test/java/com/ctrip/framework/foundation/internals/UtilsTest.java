2671e8636271eeeade66e622f9fe7faccf10116e
joshknopp <github@joshknopp.com>
2022-01-04 22:35:23 -0500

    Add test coverage for Utils

--- apollo-core/src/test/java/com/ctrip/framework/foundation/internals/UtilsTest.java
+++ apollo-core/src/test/java/com/ctrip/framework/foundation/internals/UtilsTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * @author Josh Knopp(https://github.com/joshknopp)
+ */
 class UtilsTest {
     @Nested
     class IsBlank {
